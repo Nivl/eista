ce281ed08660cd7f0e54c52e58bb0e18167ce17c
Melvin Laplanche <melvin.wont.reply@gmail.com>
2021-09-25 13:38:56 -0700

    test: add stories for Loading

---
+++ webapp/src/components/design-system/Loading/Loading.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta } from '@storybook/react';
+
+import Loading from './Loading';
+
+export const Default = () => <Loading />;
+
+export default {
+  component: Loading,
+  title: 'components/design-system/Loading',
+} as Meta;
