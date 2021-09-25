b08a5ee50c13fa6f8998c48e054d5d89d0c7286e
Melvin Laplanche <melvin.wont.reply@gmail.com>
2021-09-24 23:49:33 -0700

    feat: add 404 page

--- webapp/src/components/App/App.tsx
+++ webapp/src/components/App/App.tsx
@@ -4,11 +4,13 @@ import ProtectedRoute from 'components/ProtectedRoute';
 
 import Home from 'page/home';
 import Login from 'page/login';
+import FourOhFour from 'page/404';
 
 const App = () => (
   <Switch>
     <ProtectedRoute exact path="/" component={Home} />
     <Route path="/login" component={Login} />
+    <Route path="*" component={FourOhFour} />
   </Switch>
 );
 
