a52205732101925d802a669045becaef00d5f0cd
Melvin Laplanche <melvin.wont.reply@gmail.com>
2021-10-02 17:47:25 -0700

    refactor: default to dark mode

--- webapp/src/components/App/App.tsx
+++ webapp/src/components/App/App.tsx
@@ -1,4 +1,5 @@
 import { Auth0Provider } from '@auth0/auth0-react';
+import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -9,21 +10,22 @@ import Home from 'pages/home';
 import Login from 'pages/login';
 
 const App = () => (
-  <Router>
-    <Auth0Provider
-      domain={process.env.REACT_APP_AUTH0_DOMAIN || ''}
-      clientId={process.env.REACT_APP_AUTH0_CLIENTID || ''}
-      redirectUri={window.location.origin}
-    >
-      <ThemeProvider>
+  <ThemeProvider>
+    <CssBaseline />
+    <Router>
+      <Auth0Provider
+        domain={process.env.REACT_APP_AUTH0_DOMAIN || ''}
+        clientId={process.env.REACT_APP_AUTH0_CLIENTID || ''}
+        redirectUri={window.location.origin}
+      >
         <Switch>
           <ProtectedRoute exact path="/" component={Home} />
           <Route path="/login" component={Login} />
           <Route path="*" component={FourOhFour} />
         </Switch>
-      </ThemeProvider>
-    </Auth0Provider>
-  </Router>
+      </Auth0Provider>
+    </Router>
+  </ThemeProvider>
 );
 
 export default App;
--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,4 +1,3 @@
-import CssBaseline from '@mui/material/CssBaseline';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -7,7 +6,6 @@ import App from 'components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
@@ -16,4 +14,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// TODO(melvin): log that somewhere (it's a no-op until a method is provided)
 reportWebVitals();
