1b596a1a893f9f5b0585a18b59005513062dfa50
Melvin Laplanche <melvin.wont.reply@gmail.com>
2021-10-10 18:46:12 -0700

    add Welcome step

--- webapp/src/pages/setup/Setup.tsx
+++ webapp/src/pages/setup/Setup.tsx
@@ -1,3 +1,25 @@
-const Setup = () => <div>Setup</div>;
+import { Button, Grid, Typography } from '@mui/material';
+
+const Setup = () => (
+  <Grid
+    container
+    height="100vh"
+    direction="column"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <Typography variant="h2" gutterBottom>
+      Welcome
+    </Typography>
+
+    <Typography variant="body1" gutterBottom>
+      To get started you need to connect your bank account
+    </Typography>
+
+    <Button variant="contained" sx={{ marginTop: 2.6 }}>
+      Connect
+    </Button>
+  </Grid>
+);
 
 export default Setup;
