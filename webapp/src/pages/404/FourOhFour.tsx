2e91ce9ad8297c2c8fda4e8348d8fd36e3a074e7
Melvin <Nivl@users.noreply.github.com>
2022-01-03 00:58:07 -0800

    refactor(webapp): Implement new 404 design (#177)

--- webapp/src/pages/404/FourOhFour.tsx
+++ webapp/src/pages/404/FourOhFour.tsx
@@ -1,6 +1,39 @@
-const FourOhFour = () => (
+import { Box, Container, Typography } from '@mui/material';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const FourOhFour: NextPage = () => (
   <>
-    <div>404 - Page Not Found</div>
+    <Head>
+      <title>Page Not Found</title>
+    </Head>
+
+    <Container>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexGrow: 1,
+          height: '100vh',
+        }}
+      >
+        <Typography
+          variant="h5"
+          component="h1"
+          sx={{
+            pr: 3,
+            mr: 3,
+            borderRight: 1,
+          }}
+        >
+          404
+        </Typography>
+
+        <Typography variant="body2">This page could not be found.</Typography>
+      </Box>
+    </Container>
   </>
 );
 
