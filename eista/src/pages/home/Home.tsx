c7bab12cc07f7ac0c8bdcd93f3ce4e9ae2ee1c63
Melvin Laplanche <melvin.wont.reply@gmail.com>
2022-01-02 16:08:56 -0800

    fix(webapp): add auth check on /

--- eista/src/pages/home/Home.tsx
+++ eista/src/pages/home/Home.tsx
@@ -1,24 +1,42 @@
 import type { NextPage } from 'next';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
 
 import styles from './Home.module.css';
 import logo from './logo.svg';
+import Loading from 'components/Loader';
+import MeContext from 'contexts/MeContext';
 
-const Home: NextPage = () => (
-  <header className={styles.header}>
-    <Image src={logo} className={styles.logo} alt="logo" />
-    <p>
-      Edit <code>src/App.tsx</code> and save to reload.
-    </p>
-    <a
-      className={styles.link}
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </header>
-);
+const Home: NextPage = () => {
+  const router = useRouter();
+  const { me, isLoading: isPageLoading } = useContext(MeContext);
+
+  if (isPageLoading) {
+    return <Loading fullPage />;
+  }
+
+  if (!me) {
+    router.replace('/login');
+    return <Loading fullPage />;
+  }
+
+  return (
+    <header className={styles.header}>
+      <Image src={logo} className={styles.logo} alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className={styles.link}
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+};
 
 export default Home;
