72702fabe90e301b29925d04c11a364af118fc5c
Melvin Laplanche <melvin.wont.reply@gmail.com>
2022-01-03 00:46:54 -0800

    refactor(webapp): Implement new 404 design

--- webapp/src/pages/404/FourOhFour.stories.tsx
+++ webapp/src/pages/404/FourOhFour.stories.tsx
@@ -1,10 +1,29 @@
-import { Meta } from '@storybook/react';
+import { Meta, Story } from '@storybook/react';
+import { PropsWithChildren } from 'react';
 
 import FourOhFour from './FourOhFour';
 
-export const Default = () => <FourOhFour />;
+const Template: Story<
+  PropsWithChildren<unknown> & { theme: 'light' | 'dark' }
+> = args => <FourOhFour {...args} />;
+
+export const Default = Template.bind({});
+
+export const DarkMode = Template.bind({});
+DarkMode.args = {
+  theme: 'dark',
+};
 
 export default {
   component: FourOhFour,
   title: 'pages/FourOhFour',
+  args: {
+    theme: 'light',
+  },
+  argTypes: {
+    theme: {
+      options: ['light', 'dark'],
+      control: { type: 'radio' },
+    },
+  },
 } as Meta;
