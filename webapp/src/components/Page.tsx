b25aa9e5a79f0c23180b1a1b03a79870e0ca48ec
Melvin Laplanche <melvin.wont.reply@gmail.com>
2022-01-09 17:30:38 -0800

    uix(webapp): add top and bottom image on all pages

--- webapp/src/components/Page.tsx
+++ webapp/src/components/Page.tsx
@@ -1,11 +1,40 @@
-import { Box, Container } from '@mui/material';
+import { Box, Container, useTheme } from '@mui/material';
 import { FC } from 'react';
+
+const ScreenEdge = ({ position = 'top' }: { position?: 'top' | 'bottom' }) => {
+  const theme = useTheme();
+  const p: { top?: number | string; bottom?: number | string } = {};
+  switch (position) {
+    case 'bottom':
+      p.bottom = 0;
+      break;
+    default:
+      p.top = 0;
+  }
+
+  return (
+    <Box
+      sx={{
+        backgroundImage: `url(static/images/hero_${theme.palette.mode}.png)`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        height: '10px',
+        position: 'absolute',
+        width: '100%',
+        ...p,
+      }}
+    ></Box>
+  );
+};
 
 const Page: FC<{
   fullWidth?: boolean;
 }> = ({ children, fullWidth = false }) => (
   <Box>
+    <ScreenEdge position="top" />
     <Container maxWidth={fullWidth ? 'lg' : 'sm'}>{children}</Container>
+    <ScreenEdge position="bottom" />
   </Box>
 );
 
