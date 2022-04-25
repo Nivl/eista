b206af7a067545c4186e21a678ff1f71b3d11a24
Melvin <Nivl@users.noreply.github.com>
2022-04-24 17:22:27 -0700

    test(webapp): add stories for SignUp and Login (#231)

---
+++ webapp/src/pages/login/Login.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, Story } from '@storybook/react';
+
+import Login from './Login';
+
+const Template: Story = args => <Login {...args} />;
+
+export const Default = Template.bind({});
+
+export const DarkMode = Template.bind({});
+DarkMode.args = {
+  theme: 'dark',
+};
+
+export default {
+  component: Login,
+  title: 'pages/Login',
+  args: {
+    theme: 'light',
+  },
+  argTypes: {
+    theme: {
+      options: ['light', 'dark'],
+      control: { type: 'radio' },
+    },
+  },
+} as Meta;
